ca5635d72943eda93565e959a6df1ce720725adb
Vinith69 <ramarock09@gmail.com>
2021-09-05 10:21:01 +0530

    card for choose location

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -10,15 +10,44 @@ class LocationScreen extends StatefulWidget {
 
 class _LocationScreenState extends State<LocationScreen> {
   List locations = [
-    FetchTime(locationUrl: "Asia/Hong_Kong", location: "China(Hong Kong)")
+    FetchTime(locationUrl: "Asia/Hong_Kong", location: "China (Hong Kong)")
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Choose a location",
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Colors.black,
       ),
-      body: Text("Hello This is location choosing"),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView.builder(
+            itemCount: locations.length,
+            itemBuilder: (context, i) {
+              return SizedBox(
+                width: 200,
+                height: 100,
+                child: InkWell(
+                  splashColor: Colors.blueGrey[900],
+                  onTap: () {
+                    print(locations[i].location.toString());
+                  },
+                  child: Card(
+                    child: Center(
+                        child: Text(
+                      locations[i].location,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                    )),
+                  ),
+                ),
+              );
+            }),
+      ),
     );
   }
 }
